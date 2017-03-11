f4493e5d8646368bc3fe9efe3e4efdde67f8a245
Steve <steve@verbdb.com>
2017-03-11 13:32:24 -0600

    use html templates

--- ourtinerary-fe/src/app/app.component.ts
+++ ourtinerary-fe/src/app/app.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'app-root',
-	template: `
-		<h1>Inline Template</h1>
-
-
-	`
-
-
-	styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-
-
 export class AppComponent {
-	title = 'app works!';
+  title = 'app works!';
 }
